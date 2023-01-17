04679c82335e98fcf2982f3cba83d2564ae42899
nmorf <n.morf@yandex.ru>
2023-01-17 23:53:02 +0300

    Init commit

---
+++ src/test/java/ru/nmorf/car/backend/CarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nmorf.car.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
