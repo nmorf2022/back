c8bcb0adad4251517b46bbfb5719ae7786e4986b
nmorf <n.morf@yandex.ru>
2023-01-29 17:25:16 +0300

    CAR-3 rename controller for specification contract

--- src/main/java/ru/nmorf/car/backend/dao/controller/impl/HealthCheckControllerV1.java
+++ src/main/java/ru/nmorf/car/backend/dao/controller/impl/HealthCheckControllerV1.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/health")
-public class HealthCheckController {
+public class HealthCheckControllerV1 {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
