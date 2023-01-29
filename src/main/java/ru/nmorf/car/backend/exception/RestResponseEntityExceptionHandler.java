18a676a3309a24233543b155ef12cd190fb56dc0
nmorf <n.morf@yandex.ru>
2023-01-29 17:28:14 +0300

    CAR-3 add and delete new exceptions for exception handler

--- src/main/java/ru/nmorf/car/backend/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/ru/nmorf/car/backend/exception/RestResponseEntityExceptionHandler.java
@@ -2,24 +2,31 @@ package ru.nmorf.car.backend.exception;
 
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.nmorf.car.backend.exception.impl.JwtAuthenticationException;
+import ru.nmorf.car.backend.exception.impl.TokenValidationException;
 
 @RestControllerAdvice
 public class RestResponseEntityExceptionHandler {
 
     @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
-    @ExceptionHandler(value = {JwtAuthenticationException.class})
-    public String handleException(JwtAuthenticationException ex) {
-        return ex.getMessage();
-    }
-
-    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(value = {UsernameNotFoundException.class})
     public String handleException(UsernameNotFoundException ex) {
         return ex.getMessage();
+    }
+
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
+    @ExceptionHandler(value = {AuthenticationException.class})
+    public String handleException(AuthenticationException ex) {
+        return "Invalid email/password combination";
+    }
+
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = {TokenValidationException.class})
+    public String handleException(TokenValidationException ex) {
+        return "Authentication problems";
     }
 }
