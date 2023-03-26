00b441da8cbaefb46da7eef45aee4b890c196378
nmorf <n.morf@yandex.ru>
2023-03-26 23:28:31 +0300

    CAR-9 Add new Exception for not existing email

--- src/main/java/ru/nmorf/car/backend/service/impl/EntrantService.java
+++ src/main/java/ru/nmorf/car/backend/service/impl/EntrantService.java
@@ -1,12 +1,12 @@
 package ru.nmorf.car.backend.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.nmorf.car.backend.dao.repository.IEntrantEntityRepo;
 import ru.nmorf.car.backend.dao.repository.mapper.IEntrantEntityMapper;
 import ru.nmorf.car.backend.entity.Entrant;
+import ru.nmorf.car.backend.exception.impl.UserNotFoundException;
 import ru.nmorf.car.backend.service.IEntrantService;
 
 @Service
@@ -26,7 +26,7 @@ public class EntrantService implements IEntrantService {
     @Transactional
     public Entrant patchEntrant(Entrant info) {
         int rows = repo.patch(info.getName(), info.getDriverCategory(), info.getEmail());
-        if(rows == 0) throw new UsernameNotFoundException("User with this email not found");
+        if(rows == 0) throw new UserNotFoundException();
         return mapper.toEntrant(repo.findByEmail(info.getEmail()));
     }
 }
